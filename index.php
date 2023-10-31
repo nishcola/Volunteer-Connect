8b25263975d7e4fdc3dc07fbc25d4a45b337ba52
nishcola <144401361+nishcola@users.noreply.github.com>
2023-10-30 20:41:23 -0400

    updated landing; removed bad sections; will update

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="landing.css">
-    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Wheelchair_symbol.svg/898px-Wheelchair_symbol.svg.png">
+    <link rel="icon"
+        href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Wheelchair_symbol.svg/898px-Wheelchair_symbol.svg.png">
     <title>Disability Match - Home</title>
 </head>
+
 <body>
     <div class="container">
         <div class="navbar">
@@ -18,34 +20,14 @@
                 <a href="#">Contact</a>
             </div>
             <div class="greeting">
-                <p>Welcome, User! <a href="#">Log Out</a></p>
+                <p>Welcome, User! <a href="#" style="text-decoration: underline;">Log Out</a></p>
             </div>
         </div>
-        <div class="content">
-            <h1>Disability Match</h1>
-            <p>We believe in connecting volunteers with people who require aid.</p>
-            <a href="#company-info" class="button">Get Started</a>
-        </div>
-        <div class="company-info">
-            <div class="company-info-text">
-                <h2>Our Story</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel orci porta non pulvinar neque laoreet suspendisse interdum. Risus nullam eget felis eget nunc lobortis mattis. Amet justo donec enim diam vulputate ut pharetra sit amet. Tortor dignissim convallis aenean et. Urna duis convallis convallis tellus id interdum velit. Platea dictumst vestibulum rhoncus est pellentesque elit. Tortor id aliquet lectus proin nibh nisl condimentum id. Etiam tempor orci eu lobortis. Adipiscing enim eu turpis egestas pretium. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo.
-
-Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Nibh venenatis cras sed felis eget velit aliquet sagittis. Sit amet consectetur adipiscing elit ut aliquam purus. Tellus integer feugiat scelerisque varius morbi enim. Tellus pellentesque eu tincidunt tortor aliquam nulla. Proin fermentum leo vel orci porta non pulvinar neque laoreet. At auctor urna nunc id cursus. Aliquet risus feugiat in ante metus. Sit amet venenatis urna cursus eget nunc. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Sit amet luctus venenatis lectus magna fringilla urna porttitor. Sociis natoque penatibus et magnis dis parturient. Neque aliquam vestibulum morbi blandit cursus risus at ultrices. Ut tortor pretium viverra suspendisse potenti. Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut. In est ante in nibh. Auctor eu augue ut lectus arcu bibendum at varius vel. Cursus in hac habitasse platea dictumst quisque sagittis.
-
-Placerat in egestas erat imperdiet sed euismod nisi porta. Eget magna fermentum iaculis eu non. Ut aliquam purus sit amet. Sed arcu non odio euismod lacinia at quis. Risus ultricies tristique nulla aliquet enim tortor at auctor urna. Cursus turpis massa tincidunt dui. Tellus orci ac auctor augue. Et magnis dis parturient montes nascetur ridiculus mus mauris. A scelerisque purus semper eget duis at tellus at urna. Adipiscing elit pellentesque habitant morbi tristique senectus et. Amet mauris commodo quis imperdiet massa tincidunt nunc. Nisi vitae suscipit tellus mauris a diam maecenas. Nunc aliquet bibendum enim facilisis gravida neque convallis a. Proin sed libero enim sed faucibus turpis in. In cursus turpis massa tincidunt. Curabitur vitae nunc sed velit dignissim sodales ut eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra magna ac placerat vestibulum lectus. Pharetra pharetra massa massa ultricies mi quis. Ac ut consequat semper viverra nam libero justo laoreet. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Sit amet aliquam id diam maecenas ultricies mi. Id volutpat lacus laoreet non curabitur gravida arcu ac. Leo duis ut diam quam nulla porttitor massa id neque. Nisi vitae suscipit tellus mauris a diam. Odio facilisis mauris sit amet massa vitae tortor condimentum.</p>
-            </div>
-            <div class="company-info-image" id="company-info">
-                <img src="https://images.pexels.com/photos/6646952/pexels-photo-6646952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Company Image">
-            </div>
-        </div>
-        <div class="another-section">
-            <div class="another-section-image">
-                <img src="https://images.pexels.com/photos/3095954/pexels-photo-3095954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Another Section Image">
-            </div>
-            <div class="another-section-text">
-                <h2>Why us?</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel orci porta non pulvinar neque laoreet suspendisse interdum. Risus nullam eget felis eget nunc lobortis mattis. Amet justo donec enim diam vulputate ut pharetra sit amet. Tortor dignissim convallis aenean et. Urna duis convallis convallis tellus id interdum velit. Platea dictumst vestibulum rhoncus est pellentesque elit. Tortor id aliquet lectus proin nibh nisl condimentum id. Etiam tempor orci eu lobortis. Adipiscing enim eu turpis egestas pretium. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis. Nibh venenatis cras sed felis eget velit aliquet sagittis. Sit amet consectetur adipiscing elit ut aliquam purus. Tellus integer feugiat scelerisque varius morbi enim. Tellus pellentesque eu tincidunt tortor aliquam nulla. Proin fermentum leo vel orci porta non pulvinar neque laoreet. At auctor urna nunc id cursus. Aliquet risus feugiat in ante metus. Sit amet venenatis urna cursus eget nunc. Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at augue. Sit amet luctus venenatis lectus magna fringilla urna porttitor. Sociis natoque penatibus et magnis dis parturient. Neque aliquam vestibulum morbi blandit cursus risus at ultrices. Ut tortor pretium viverra suspendisse potenti. Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut. In est ante in nibh. Auctor eu augue ut lectus arcu bibendum at varius vel. Cursus in hac habitasse platea dictumst quisque sagittis. Placerat in egestas erat imperdiet sed euismod nisi porta. Eget magna fermentum iaculis eu non. Ut aliquam purus sit amet. Sed arcu non odio euismod lacinia at quis. Risus ultricies tristique nulla aliquet enim tortor at auctor urna. Cursus turpis massa tincidunt dui. Tellus orci ac auctor augue. Et magnis dis parturient montes nascetur ridiculus mus mauris. A scelerisque purus semper eget duis at tellus at urna. Adipiscing elit pellentesque habitant morbi tristique senectus et. Amet mauris commodo quis imperdiet massa tincidunt nunc. Nisi vitae suscipit tellus mauris a diam maecenas. Nunc aliquet bibendum enim facilisis gravida neque convallis a. Proin sed libero enim sed faucibus turpis in. In cursus turpis massa tincidunt. Curabitur vitae nunc sed velit dignissim sodales ut eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra magna ac placerat vestibulum lectus. Pharetra pharetra massa massa ultricies mi quis. Ac ut consequat semper viverra nam libero justo laoreet. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Sit amet aliquam id diam maecenas ultricies mi. Id volutpat lacus laoreet non curabitur gravida arcu ac. Leo duis ut diam quam nulla porttitor massa id neque. Nisi vitae suscipit tellus mauris a diam. Odio facilisis mauris sit amet massa vitae tortor condimentum.</p>
+        <div style="background-image: url(images/pexels-rdne-stock-project-6647037.jpg);background-size:cover;">
+            <div class="content">
+                <h1>Disability Match</h1>
+                <p>We believe in connecting volunteers with people who require aid.</p>
+                <a href="#company-info" class="button">Get Started</a>
             </div>
         </div>
         <div class="footer">
