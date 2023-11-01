b7a0b39cbff9bbf5863993ea9bd4393347f3294c
Nish <144401361+nishcola@users.noreply.github.com>
2023-11-01 14:22:33 -0400

    updated signup complete page css

--- SignupComplete.php
+++ SignupComplete.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Wheelchair_symbol.svg/898px-Wheelchair_symbol.svg.png">
     <script defer src="signup.js"></script>
+    <link rel="stylesheet" href="login.css">
     <title>Sign Up Successful!</title>
 </head>
 <body>
-    Welcome: 
-    <p id="scUsername"></p><br> 
+    <div class="background-container">
+        <div class="bgc2">
+            <p>Welcome, </p><p id="scUsername"></p>
+        </div>
+    </div>
 </body>
