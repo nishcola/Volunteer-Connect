304c6d71005ec3e35a70a4561951b8a7bf008755
Inconquerable <rammakki431@gmail.com>
2023-11-04 16:18:11 -0400

    fixed app.js messup

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
