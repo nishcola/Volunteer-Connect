8cafba122df16dfc3ef240e3684551f0636aca79
nishcola <144401361+nishcola@users.noreply.github.com>
2023-11-03 22:19:27 -0400

    sticky navbar, new section on homepage
    check it out

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
--- index.php
+++ index.php
@@ -13,12 +13,12 @@
 
 <body>
     <div class="container">
-        <div class="navbar">
+        <div class="navbar sticky" id="navbar">
             <div class="links">
                 <a href="#">Home</a>
                 <a href="#about-heading">About</a>
-                <a href="#">Services</a>
-                <a href="#">Contact</a>
+                <a href="#services">Services</a>
+                <a href="#footer">Contact</a>
             </div>
             <div class="greeting">
                 <p><a href="signup.php" style="text-decoration: underline;">Sign Up</a><a href="login.php" style="text-decoration: underline;">Login</a></p>
@@ -34,16 +34,35 @@
         <div style="background-image: url('images/pexels-rdne-stock-project-6646952.jpg');background-size: cover;">
             <div class="about-flex-container" id="about-heading">
                 <div class="about-div">
-                    <h1 class="about-heading">About Us</h1>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi ab ducimus esse ullam, vel
+                    <h1 class="about-heading hidden">About Us</h1>
+                    <p class="hidden">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi ab ducimus esse ullam, vel
                         tenetur.
                         Recusandae id porro rem quibusdam, accusamus voluptates asperiores provident culpa quam dolorem
                         animi odio obcaecati?</p>
-                    <a href="about.php"><button class="homepage-button" id="goToAbout">Learn More</button></a>
                 </div>
             </div>
         </div>
-        <div class="footer">
+        <div style="background-image: url(images/pexels-rdne-stock-project-6646877.jpg);background-size: cover;">
+            <div class="services-flex-container" id="services">
+                <div class="services-div-1">
+                    <h1 class="services hidden">Our Services</h1>
+                    <p class="hidden">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, expedita numquam? Aut in, 
+                        optio quaerat quas et corporis reprehenderit quia, tempora repellat nisi enim hic ex dolorum, modi reiciendis necessitatibus.
+                    </p>
+                    <a href="services.php"><button class="homepage-button" id="goToServices">Start Now</button></a>
+                </div>
+                <div class="line"></div>
+                <div class="services-div-2">
+                    <h1 class="services hidden">Our Guarantee</h1>
+                    <p class="hidden">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id et impedit aliquid quaerat ipsum harum sit
+                        exercitationem tempora quo magnam iure perferendis corporis vitae accusantium porro libero, assumenda blanditiis repellat.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="footer" id="footer">
             <p>We prioritize customer experience. Email us with questions, doubts, or feedback, at: lorem@ipsum.dolorsitamet</p>
         </div>
     </div>
