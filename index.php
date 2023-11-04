7fbc0f427e12d60fd4205272cfdfce13efd4e3d8
nishcola <144401361+nishcola@users.noreply.github.com>
2023-11-03 20:03:24 -0400

    updated homepage

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="landing.css">
-    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Wheelchair_symbol.svg/898px-Wheelchair_symbol.svg.png">
+    <link rel="icon"
+        href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Wheelchair_symbol.svg/898px-Wheelchair_symbol.svg.png">
     <script defer src="app.js"></script>
     <title>Disability Match - Home</title>
 </head>
@@ -20,41 +21,30 @@
                 <a href="#">Contact</a>
             </div>
             <div class="greeting">
-                <p>Welcome, User! <a href="#" style="text-decoration: underline;">Log Out</a></p>
+                <p><a href="signup.php" style="text-decoration: underline;">Sign Up</a><a href="#" style="text-decoration: underline;">Log Out</a></p>
             </div>
         </div>
         <div style="background-image: url(images/pexels-rdne-stock-project-6647037.jpg);background-size:cover;">
-            <div class="content">
+            <div class="landing-header">
                 <h1>Disability Match</h1>
                 <p>We believe in connecting volunteers with people who require aid.</p>
-                <a href="#company-info" class="button">Get Started</a>
+                <a href="#about-heading" class="button">Get Started</a>
             </div>
         </div>
-        <section class="landing-content">
-            <h1 class="hidden">About Us</h1>
-            <p class="hidden">Our company is passionate about making a positive impact on people's lives. Our software was born out of the desire to bridge the gap between those in need and those willing to lend a helping hand. Our app and company includes many aspects that separate us from the competition. Firstly, we have a mission-driven approach. Our primary mission with our app is to connect individuals with volunteers who genuinely want to make a difference. We believe that everyone deserves support, and we are committed to facilitating meaningful connections that can transform lives. Besides that, we as a company are deeply rooted in our community and strive to foster a sense of togetherness. By connecting individuals with local volunteers, we aim to strengthen community bonds and create a more caring and supportive environment for everyone. Finally, we are dedicated to constantly improving our software and services. We actively seek feedback from our users and volunteers, allowing us to adapt and enhance our platform to better serve their needs. We value innovation and are constantly exploring new ways to optimize the matching process, to create a better experience for all. </p>
-            <div class="bodyimage-container">
-                <img class="hidden" id="delay1" src="https://images.pexels.com/photos/18721030/pexels-photo-18721030/free-photo-of-church-tower-behind-trees.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay2" src="https://images.pexels.com/photos/3095954/pexels-photo-3095954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay3" src="https://images.pexels.com/photos/4064339/pexels-photo-4064339.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay4" src="https://images.pexels.com/photos/4063513/pexels-photo-4063513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay5" src="https://images.pexels.com/photos/3101214/pexels-photo-3101214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
+        <div style="background-image: url('images/pexels-rdne-stock-project-6646952.jpg');background-size: cover;">
+            <div class="about-flex-container" id="about-heading">
+                <div class="about-div">
+                    <h1 class="about-heading">About Us</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi ab ducimus esse ullam, vel
+                        tenetur.
+                        Recusandae id porro rem quibusdam, accusamus voluptates asperiores provident culpa quam dolorem
+                        animi odio obcaecati?</p>
+                    <a href="about.php"><button class="homepage-button" id="goToAbout">Learn More</button></a>
+                </div>
             </div>
-        </section>
-        <section class="landing-content">
-            <h1 class="hidden">Why Choose Us?</h1>
-            <p class="hidden">At our company, we take pride in our software that aims to connect people in need with volunteers who are eager to help. Here are some compelling reasons why you should choose our platform. To start, we have an extensive volunteer database. We have assembled a vast and diverse network of volunteers from various backgrounds and skill sets. Our database ensures that we can find the right match for individuals seeking assistance, allowing us to provide tailored support to people all across Pennsylvania. Additionally, we use efficient and effective matching in our software. The software utilizes advanced algorithms to match individuals with volunteers based on their specific requirements. By considering factors such as location, availability, and skills, we ensure that the connection between the volunteer and the person in need is true and helpful. Another reason is that our company is trustworthy and reliable. We understand the importance of trust when it comes to connecting people with volunteers. That's why we have implemented a rigorous screening process for all our volunteers. Background checks and reference verifications are conducted to ensure the safety and reliability of our volunteers in their interactions with people in need. Lastly, our platform is designed with user convenience in mind. We have created an intuitive and easy-to-use interface that allows individuals to seamlessly request assistance, browse available volunteers, and communicate effectively. Our goal is to make the experience as smooth as possible for both those seeking help and those offering it, in order to create a better world for all. </p>
-
-            <div class="bodyimage-container">
-                <img class="hidden" id="delay1pt2" src="https://images.pexels.com/photos/18721030/pexels-photo-18721030/free-photo-of-church-tower-behind-trees.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay2pt2" src="https://images.pexels.com/photos/3095954/pexels-photo-3095954.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay3pt2" src="https://images.pexels.com/photos/4064339/pexels-photo-4064339.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay4pt2" src="https://images.pexels.com/photos/4063513/pexels-photo-4063513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-                <img class="hidden" id="delay5pt2" src="https://images.pexels.com/photos/3101214/pexels-photo-3101214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image">
-            </div>
-        </section>
+        </div>
         <div class="footer">
-            <p>We prioritize customer experience. Email us at: lorem@ipsum.dolorsitamet</p>
+            <p>We prioritize customer experience. Email us with questions, doubts, or feedback, at: lorem@ipsum.dolorsitamet</p>
         </div>
     </div>
 </body>
