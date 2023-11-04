4c9227005b875b89576e2b0e08ac67374d80df44
Tharun Thangavel <tharunthangavel@outlook.com>
2023-11-04 17:48:09 -0400

    Added SQL commit

--- SignUpRedirect.php
+++ SignUpRedirect.php
@@ -6,9 +6,40 @@
     <title>Signing In...</title>
 </head>
 <body>
-    type: <?php echo $_POST["accountType"]; ?><br>
-    first name: <?php echo $_POST["firstName"]; ?><br>
-    last name: <?php echo $_POST["lastName"]; ?><br>
-    zip code: <?php echo $_POST["zipCode"]; ?>
+    <p id="accountType">type: <?php echo $_POST["accountType"]; ?></p>
+    <p id="firstName">first name: <?php echo $_POST["firstName"]; ?></p>
+    <p id="lastName">last name: <?php echo $_POST["lastName"]; ?></p>
+    <p id="zipCode">zip code: <?php echo $_POST["zipCode"]; ?></p>
+
+    <?php
+        $sqlservername = "localhost";
+        $sqlusername = "root";
+        $sqlpassword = "";
+        $sqldbname = "disabilitymatch";
+
+        $Uusername = $_COOKIE["username"];
+        $Upassword = $_COOKIE["password"];
+        $UaccountType = $_POST["accountType"];
+        $UfirstName = $_POST["firstName"];
+        $UlastName = $_POST["lastName"];
+        $UzipCode = $_POST["zipCode"];
+
+        // Create connection
+        $conn = mysqli_connect($sqlservername, $sqlusername, $sqlpassword, $sqldbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        echo "Connected successfully";
+
+        $sql = "INSERT INTO userrecords (Username, Password, AccountType, ZipCode, FirstName, LastName)
+        VALUES ('$Uusername', '$Upassword', '$UaccountType', '$UzipCode', '$UfirstName', '$UlastName')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    ?>
 </body>
 </html>
