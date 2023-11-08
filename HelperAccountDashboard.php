5ded31484738030d69834857826dc76124bf5218
nishcola <144401361+nishcola@users.noreply.github.com>
2023-11-07 19:33:32 -0500

    dashboard stuff

--- HelperAccountDashboard.php
+++ HelperAccountDashboard.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="AccountDashboard.css">
+    <script src="https://kit.fontawesome.com/bf12c23961.js" crossorigin="anonymous"></script>
     <script defer src="HelperAccountDashboard.js"></script>
     <title>Account Dashboard</title>
 </head>
@@ -22,9 +23,21 @@
                 <p><a href="#" style="text-decoration: underline;" id="logoutLink">Log Out</a></p>
             </div>
         </div>
-        
-        <div class>
-
+        <div class="uad-left-block">
+            <div class="uad-left-block-content">
+                <img id="pfp" src='images/115-1150152_default-profile-picture-avatar-png-green.png'>
+                <h1 id="username">Username</h1>
+                <div id="account-info">
+                    <i class="fa-solid fa-envelope"></i><label>Email: lorem@ipsum.dolor</label>
+                    <br />
+                    <i class="fa-solid fa-handshake"></i><label>Type: Helper</label>
+                    <br />
+                    <i class="fa-solid fa-house"></i><label>Zip Code: 17050</label>
+                    <br />
+                    <i class="fa-solid fa-lock"></i><label>Password: e********</label>
+                    <br />
+                </div>
+            </div>
         </div>
     </div>
 </body>
