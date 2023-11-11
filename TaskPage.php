f3e9aec1aa3c90cd19f2fd0761a841588d8e6722
Tharun Thangavel <tharunthangavel@outlook.com>
2023-11-11 13:19:41 -0500

    Task page

--- TaskPage.php
+++ TaskPage.php
@@ -25,7 +25,7 @@
     <div class="uad-left-block">
         <div class="uad-left-block-content">
             <img id="pfp" src='images/115-1150152_default-profile-picture-avatar-png-green.png'>
-            <h1 id="username">Task created by: <br />Username</h1>
+            <h1 id="username">Opportunity Provided By: <br />Username</h1>
             <div class="task-information">
                 <p><i class="fa-solid fa-calendar-days"></i> Date: </p>
                 <p><i class="fa-solid fa-hourglass-start"></i> Start Time:</p>
@@ -35,6 +35,34 @@
                 <button class="task-info-button" id="complete">Mark As Complete.</button>
             </div>
         </div>
+    </div>
+    <div class="uad-right-block">
+        <h1 class = 'title'>Hello Beta!</h1>
+            <p class = 'task-description-title'><b>Description: </b></p>
+            <p class = 'task-description'> cooking up a tsa projec tlmao</p>
+            <h2 class = 'task-description'>Address: UP ur butt lol</h1>
+            <h2 class = 'task-description'>Zip-Code: UP ur butt lol</h1>
+
+        <table style = "width:95%" id="taskTable" class = 'task-description'>
+            <tbody id="taskTableBody">
+                <tr  style = "width:100%" class = 'task-description-title'>
+                    <th>Volenteers: </th>
+                    <th style = "width:12%">Options</th>
+                </tr>
+                <tr>
+                    <td>Tharrrruuuuunnnnn</td>
+                    <td><button class="volenteer-Remove-button">Remove</button><button class="volenteer-Completed-button">Completed</button</td>
+                </tr>
+                <tr>
+                    <td>Ram</td>
+                    <td><button class="volenteer-Remove-button">Remove</button><button class="volenteer-Completed-button">Completed</button</td>
+                </tr>
+                <tr>
+                    <td>nish_cola</td>
+                    <td><button class="volenteer-Remove-button">Remove</button><button class="volenteer-Completed-button">Completed</button</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 
