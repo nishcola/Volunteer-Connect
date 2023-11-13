8b17cdff79c33fb8e1fda3d293b06c24393a51b6
nishcola <144401361+nishcola@users.noreply.github.com>
2023-11-13 14:13:55 -0500

    Update CreateTask.php

--- CreateTask.php
+++ CreateTask.php
@@ -9,11 +9,11 @@
 </head>
 
 <body>
-    <div class="background-container">
-        <div class="bgc2">
-            <form method="post" action="CreateTaskRedirect.php">
+    <div class="background-container" style="height: 140vh;">
+        <div class="bgc2" style="height: 140vh;">
+            <form method="post" action="CreateTaskRedirect.php" style="margin-top: 10vh;">
                 <div>
-                    <h3 id="scUsername"><em>
+                    <h3 id="scUsername">Welcome, <em>
                             <?php $username = 'username';
                             echo $_COOKIE[$username]; ?>
                         </em></h3>
@@ -30,24 +30,24 @@
                 </div>
                 <div>
                     <p>Date of Task</p>
-                    <input id="date" type="date" name="date" autocomplete="on" placeholder="Date of the Task..." />
+                    <input id="date" type="date" name="date" autocomplete="on" placeholder="Date of the Task..." style="width: 93.5%; padding: 10px; border-radius: 5px; border: none;"/>
 
                     <p>Task Start Time:</p>
                     <input id="time" type="time" name="startTime" autocomplete="on"
-                        placeholder="When the task starts..." />
+                        placeholder="When the task starts..." style="width: 93.5%; padding: 10px; border-radius: 5px; border: none;" />
 
                     <p>Task End Time:</p>
-                    <input id="time" type="time" name="endTime" autocomplete="on" placeholder="When the task ends..." />
+                    <input id="time" type="time" name="endTime" autocomplete="on" placeholder="When the task ends..." style="width: 93.5%; padding: 10px; border-radius: 5px; border: none;" />
 
                     <p>Slots Available</p>
-                    <input type="number" name="maxSlots" step="1" min="1"></input>
+                    <input type="number" name="maxSlots" step="1" min="1" style="width: 93.5%; padding: 10px; border-radius: 5px; border: none;"></input>
 
                     <p>Address:</p>
                     <input id="taskAddress" type="text" name="taskAddress" autocomplete="on"
-                        placeholder="Street address..." />
+                        placeholder="Street address..." style="width: 100%; padding: 10px; border-radius: 5px; border: none;"/>
 
                     <p>Zip Code:</p>
-                    <input id="taskZipCode" type="number" name="taskZipCode" autocomplete="on"/>
+                    <input id="taskZipCode" type="number" name="taskZipCode" autocomplete="on" style="width: 93.5%; padding: 10px; border-radius: 5px; border: none;"/>
                 </div>
                 <input type="submit" value="Submit">
             </form>
